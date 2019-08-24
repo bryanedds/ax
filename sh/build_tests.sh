b6edba00d78717481943cb9d2bc38fe730545229
bryanedds <bryanedds@gmail.com>
2019-08-24 14:03:11 -0400

    Enabled -fpermissive in travis build.

--- sh/build_tests.sh
+++ sh/build_tests.sh
@@ -1,4 +1,4 @@
 CPP_FILES="../src/cpp/tests/tests.cpp "
 mkdir -p ./bin
-g++ -std=c++17 -Wall -Wextra -pedantic -g -O2 -pthread -march=native -D BLAH_NO_THREAD_SUPPORT $CPP_FILES ./bin/libax.a -o ./bin/ax_tests.exe
+g++ -std=c++17 -Wall -Wextra -pedantic -fpermissive -g -O2 -pthread -march=native -D BLAH_NO_THREAD_SUPPORT $CPP_FILES ./bin/libax.a -o ./bin/ax_tests.exe
 
