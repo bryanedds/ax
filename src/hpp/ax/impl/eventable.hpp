fb550f5070271b49d22ea92cee583d34c0a930f5
bryanedds <bryanedds@gmail.com>
2019-08-28 23:45:38 -0400

    Started working on ax::stream.

--- src/hpp/ax/impl/eventable.hpp
+++ src/hpp/ax/impl/eventable.hpp
@@ -116,6 +116,52 @@ namespace ax
         ax::subscriptions_map subscriptions_map;
         ax::unsubscription_map unsubscription_map;
     };
+
+    template<typename W>
+    using unsubscribe = std::function<void(W&)> fn;
+
+    template<typename W>
+    struct subscribe
+    {
+        ax::address address;
+        ax::unsubscribe<W> unsubscribe;
+    };
+
+    template<typename T, typename W>
+    struct stream
+    {
+        stream make_empty()
+        {
+            CONSTRAIN(W, ax::eventable);
+            VAL subscribe = [](W& w) { return ax::subscribe { address{}, [](W&){} }; }
+            return stream { subscribe };
+        }
+
+        stream make(ax::address event_address)
+        {
+            CONSTRAIN(W, ax::eventable);
+            VAL subscribe = [event_address](W& w)
+            {
+                VAL global_participant = w.get_global_participant();
+                VAL subscription_key = std::uuid::make_uuid();
+                VAL subscription_address = ax::address(subscription_key.to_string());
+                VAL unsubscribe = [](W& w) { return w.unsubscribe<W>(subscription_key); };
+                VAL subscription = [](const ax::event<T>& evt)
+                {
+                    // TODO: event tracing
+                    // TODO: subscription sorting
+                    // TODO: allow wildcard flag (= false)
+                    // TODO: event cascading (= cascade)
+                    w.publish_event<T>(evt.data, subscription_address, global_participant);
+                };
+                w.subscribe_event<T>(subscription_key, event_address, global_participant);
+                return ax::subscribe{ subscription_address, unsubscribe };
+            }
+            return ax::stream{ subscribe };
+        }
+
+        std::function<ax::subscribe(W&)>;
+    };
 }
 
 #endif
